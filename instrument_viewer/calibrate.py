756d27ecf699c3888c413c76f743b84b291f6008
Donald Boyce <donald.e.boyce@gmail.com>
2017-06-14 11:45:27 -0400

    separated display plane into new file

--- instrument_viewer/calibrate.py
+++ instrument_viewer/calibrate.py
@@ -5,8 +5,6 @@ from matplotlib import pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
 from hexrd.gridutil import cellIndices
-from hexrd.xrd import transforms_CAPI as xfcapi
-from hexrd import instrument
 from hexrd import imageseries
 from make_pd_patch import generate_tth_eta as polar_view
 
@@ -15,6 +13,8 @@ from skimage import transform as tf
 from skimage.exposure import equalize_adapthist
 
 Pimgs = imageseries.process.ProcessedImageSeries
+
+from display_plane import DisplayPlane
 
 class InstrumentViewer(object):
 
@@ -41,6 +41,7 @@ class InstrumentViewer(object):
         self.show_image()
         plt.show()
 
+    # ========== Set up
     def _load_panels(self):
         self.panel_ids = self.instr._detectors.keys()
         self.panels = self.instr._detectors.values()
@@ -81,6 +82,7 @@ class InstrumentViewer(object):
         self.dpanel = self.dplane.display_panel(self.dpanel_sizes,
                                                 self.pixel_size)
 
+    # ========== GUI
     def set_interactors(self):
         self._figure.canvas.mpl_connect('key_press_event', self.onkeypress)
 
@@ -246,6 +248,7 @@ class InstrumentViewer(object):
 
         self.show_image()
 
+    # ========== Drawing
     def draw_polar(self):
         """show polar view of rings"""
         pv = polar_view(self.planeData, self.instr)
@@ -374,57 +377,3 @@ class InstrumentViewer(object):
         hkl = str(self.planeData.getHKLs(asStr=True, allHKLs=True, thisTTh=tth))
         return "x=%.2f, y=%.2f, d=%.3f tth=%.2f eta=%.2f HKLs=%s" \
           % (xy_data[0, 0], xy_data[0, 1], dsp, np.degrees(tth), np.degrees(eta), hkl)
-
-    pass
-
-
-class DisplayPlane(object):
-
-    def __init__(self):
-        self.tilt = np.zeros(3)
-        self.rmat = xfcapi.makeDetectorRotMat(self.tilt)
-        self.tvec = np.r_[0., 0., -1000.]
-
-    def panel_size(self, instr):
-        """return bounding box of instrument panels in display plane"""
-        xmin_i = ymin_i = np.inf
-        xmax_i = ymax_i = -np.inf
-        for detector_id in instr._detectors:
-            panel = instr._detectors[detector_id]
-            # find max extent
-            corners = np.vstack(
-                [panel.corner_ll,
-                 panel.corner_lr,
-                 panel.corner_ur,
-                 panel.corner_ul,
-                 ]
-            )
-            tmp = panel.map_to_plane(corners, self.rmat, self.tvec)
-            xmin, xmax = np.sort(tmp[:, 0])[[0, -1]]
-            ymin, ymax = np.sort(tmp[:, 1])[[0, -1]]
-
-            xmin_i = min(xmin, xmin_i)
-            ymin_i = min(ymin, ymin_i)
-            xmax_i = max(xmax, xmax_i)
-            ymax_i = max(ymax, ymax_i)
-            pass
-
-        del_x = 2*max(abs(xmin_i), abs(xmax_i))
-        del_y = 2*max(abs(ymin_i), abs(ymax_i))
-
-        return (del_x, del_y)
-
-    def display_panel(self, sizes, mps):
-
-        del_x = sizes[0]
-        del_y = sizes[1]
-
-        ncols_map = int(del_x/mps)
-        nrows_map = int(del_y/mps)
-
-        display_panel = instrument.PlanarDetector(
-            rows=nrows_map, cols=ncols_map,
-            pixel_size=(mps, mps),
-            tvec=self.tvec, tilt=self.tilt)
-
-        return display_panel
