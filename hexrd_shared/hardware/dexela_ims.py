7cbef60d96ac50583b345617800dd2222ccad15d
Donald Boyce <donald.e.boyce@gmail.com>
2018-04-24 17:35:46 -0400

    replaced pixfix with add-row and add-column ops

--- hexrd_shared/hardware/dexela_ims.py
+++ hexrd_shared/hardware/dexela_ims.py
@@ -6,40 +6,36 @@ from hexrd.imageseries.process import ProcessedImageSeries
 
 class ProcessedDexelaIMS(ProcessedImageSeries):
 
-    PIXFIX = 'pixfix'
+    ADDROW = 'add-row'
+    ADDCOL = 'add-column'
 
     def __init__(self, imser, oplist, **kwargs):
         super(ProcessedDexelaIMS, self).__init__(imser, oplist, **kwargs)
-        self.addop(self.PIXFIX, self._pixfix)
+        self.addop(self.ADDROW, self._addrow)
+        self.addop(self.ADDCOL, self._addcol)
 
-        # You could automatically apply the pixfix if you want
-        self._oplist = [(self.PIXFIX, None)] + self.oplist
+    def _addrow(self, img, k):
+        """insert row into position k"""
+        shp = img.shape
+        pimg = np.insert(img, k, 0, axis=0)
+        if k==0:
+            pimg[0] = pimg[1]
+        elif k==shp[0]:
+            pimg[k] = pimg[k-1]
+        else: # in middle
+            pimg[k] = (pimg[k-1] + pimg[k+1])/2
 
-    def _pixfix(self, img, ddummy):
-        pimg=img
+        return pimg
 
-        rows=pimg.shape[0]
-        cols=pimg.shape[1]
-
-        pimg=np.insert(pimg,rows/2,0,axis=0)
-        pimg=np.insert(pimg,cols/2,0,axis=1)
-
-
-        pimg[rows/2,1:-1]=(pimg[rows/2-1,0:-2]+pimg[rows/2+1,0:-2]\
-                           +pimg[rows/2-1,2:]+pimg[rows/2+1,2:])/8\
-                           +(pimg[rows/2-1,1:-1]+pimg[rows/2+1,1:-1])/4
-        pimg[1:-1,cols/2]=(pimg[0:-2,cols/2-1]+pimg[0:-2,cols/2+1]\
-                           +pimg[2:,cols/2-1]+pimg[2:,cols/2+1])/8\
-                           +(pimg[1:-1,cols/2-1]+pimg[1:-1,cols/2+1])/4
-
-        #special cases
-        pimg[rows/2,cols/2]=(pimg[rows/2-1,cols/2-1]\
-                             +pimg[rows/2-1,cols/2+1]\
-                             +pimg[rows/2+1,cols/2-1]\
-                             +pimg[rows/2+1,cols/2+1])/4#center
-        pimg[0,cols/2]=(pimg[0,cols/2-1]+pimg[0,cols/2+1])/2#top
-        pimg[rows/2,0]=(pimg[rows/2-1,0]+pimg[rows/2+1,0])/2#left
-        pimg[rows/2,-1]=(pimg[rows/2-1,-1]+pimg[rows/2+1,-1])/2#right
-        pimg[-1,cols/2]=(pimg[-1,cols/2-1]+pimg[0,cols/2+1])/2#bottom
+    def _addcol(self, img, k):
+        """insert row into position k"""
+        shp = img.shape
+        pimg = np.insert(img, k, 0, axis=1)
+        if k==0:
+            pimg[:,0] = pimg[:,1]
+        elif k==shp[0]:
+            pimg[:,k] = pimg[:,k-1]
+        else: # in middle
+            pimg[:,k] = (pimg[:,k-1] + pimg[:,k+1])/2
 
         return pimg
--- hexrd_shared/hardware/testing/test_dexela_ims.py
+++ hexrd_shared/hardware/testing/test_dexela_ims.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import numpy as np
 import unittest
 
@@ -8,22 +10,46 @@ from hexrd import imageseries
 class TestDexelaIMS(unittest.TestCase):
 
     def setUp(self):
-        self._nfxy = (3, 4, 4)
+        self._nfxy = (3, 6, 6)
         a = np.zeros(self._nfxy)
         ind = np.array([0,1,2])
         a[ind, 1,2] = 1 + ind
         self.is_a = imageseries.open(None, 'array', data=a)
 
-    def test_oplist(self):
-        dims = dexela_ims.ProcessedDexelaIMS(self.is_a, [])
-        self.assertEqual(len(dims.oplist), 1)
-
-    def test_shape(self):
-        dims = dexela_ims.ProcessedDexelaIMS(self.is_a, [])
-        self.assertEqual(dims.shape[0], self._nfxy[1] + 1)
-        self.assertEqual(dims.shape[1], self._nfxy[2] + 1)
-
     def test_dtype(self):
         dims = dexela_ims.ProcessedDexelaIMS(self.is_a, [])
         img = dims[0]
         self.assertEqual(dims.dtype, img.dtype)
+
+    def test_shape_rows(self):
+        dims = dexela_ims.ProcessedDexelaIMS(self.is_a, [('add-row', 1)])
+        self.assertEqual(dims.shape[0], self._nfxy[1] + 1)
+
+    def test_shape_cols(self):
+        dims = dexela_ims.ProcessedDexelaIMS(self.is_a, [('add-column', 1)])
+        self.assertEqual(dims.shape[1], self._nfxy[2] + 1)
+
+    def test_addrow_avg(self):
+        op = ('add-row', 1)
+        a = np.linspace(0, 3, 4, dtype=np.uint).reshape((1,2,2))
+        ims_a = imageseries.open(None, 'array', data=a)
+        dims = dexela_ims.ProcessedDexelaIMS(ims_a, [op])
+        self.assertEqual(dims[0][1,0], 1)
+        self.assertEqual(dims[0][1,1], 2)
+
+    def test_addcol_avg(self):
+        op = ('add-column', 1)
+        a = np.linspace(0, 3, 4, dtype=np.uint).reshape((1,2,2))
+        ims_a = imageseries.open(None, 'array', data=a)
+        dims = dexela_ims.ProcessedDexelaIMS(ims_a, [op])
+        self.assertEqual(dims[0][0,1], 0)
+        self.assertEqual(dims[0][1,1], 2)
+
+    def test_commute(self):
+        opr = ('add-row', 1)
+        opc = ('add-column', 1)
+        a = np.linspace(0, 3, 4, dtype=np.uint).reshape((1,2,2))
+        ims_a = imageseries.open(None, 'array', data=a)
+        dims_rc = dexela_ims.ProcessedDexelaIMS(ims_a, [opr, opc])
+        dims_cr = dexela_ims.ProcessedDexelaIMS(ims_a, [opc, opr])
+        self.assertEqual(dims_rc[0][1,1], dims_cr[0][1,1])
