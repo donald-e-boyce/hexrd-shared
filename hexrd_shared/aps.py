d8efe27665c98c18164da2ed969f0070827f5bd4
Donald Boyce <donald.e.boyce@gmail.com>
2019-07-24 11:14:59 -0400

    added scan_info(); added KeyIndex class

--- hexrd_shared/aps.py
+++ hexrd_shared/aps.py
@@ -1,15 +1,19 @@
 """Parse APS .par files"""
+from collections import namedtuple
+
 import numpy as np
 
+KeyIndex = namedtuple('KeyIndex', ['key', 'index'])
+_mtsY =  KeyIndex('mtsY', 24)
+_mtsX =  KeyIndex('mtsX', 25)
+_ostart = KeyIndex('ostart', 31)
+_ostop = KeyIndex('ostop', 32)
+_nframes = KeyIndex('nframes', 33)
+_exposure = KeyIndex('exposure', 34)
+_sample = KeyIndex('sample', 35)
+_scan = KeyIndex('scan', 36)
 
-class ParFields(object):
-    # 32, 33, 34, 36, 37 (1-based)
-    ostart = 31
-    ostop = 32
-    nframes = 33
-    name = 35
-    scan = 36
-
+_daterange = range(5) # first five fields are date
 
 class ParParser(object):
 
@@ -21,18 +25,19 @@ class ParParser(object):
         for l in self.parlines:
             self.split_lines.append(l.split())
 
-    def matchlines(self, name):
-        """return lines matching string"""
+    def _matchlines(self, sample):
+        """return line numbers matching sample"""
         result = []
-        for l in self.parlines:
-            if name in l:
-                result.append(l)
+        for lnum, sl in enumerate(self.split_lines):
+            if sl[_sample.index] == sample:
+                result.append(lnum)
+
         return result
 
     @property
     def samples(self):
         """names of samples"""
-        names = [sl[ParFields.name] for sl in self.split_lines]
+        names = [sl[_sample.index] for sl in self.split_lines]
         return np.unique(names)
 
     def scans(self, sample=None):
@@ -45,8 +50,32 @@ class ParParser(object):
                 if sample in sl:
                     slines.append(sl)
 
-        scans = [sl[ParFields.scan] for sl in slines]
+        scans = [sl[_scan.index] for sl in slines]
         return np.unique(scans)
+
+    def scan_info(self, sample):
+        """return scan info matching name
+
+        return data as a dictionary of dictionaries indexed by scan string
+"""
+        # mtsX = int(np.round(1e4*float(pars[25])))
+        # mtsY = int(np.round(1e4*float(pars[24])))
+
+        scand = dict()
+        lnums = self._matchlines(sample)
+        for l in lnums:
+            flds = self.split_lines[l]
+            scand[flds[_scan.index]] = {
+                _ostart.key: flds[_ostart.index],
+                _ostop.key: flds[_ostop.index],
+                _nframes.key: flds[_nframes.index],
+                _mtsX.key: _rounde4(flds[_mtsX.index]),
+                _mtsY.key: _rounde4(flds[_mtsY.index]),
+                _exposure.key: float(flds[_exposure.index]),
+                'date': ' '.join([flds[i] for i in _daterange])
+                }
+
+        return scand
 
     def imageseries(self, name, flips, panels=['ge1', 'ge2']):
         """generate imageseries from par file"""
@@ -56,19 +85,12 @@ class ParParser(object):
         for l in lines:
             flds = l.split()
             files += '\n' + _scanfile_tmpl.format(
-                name=name, num=flds[Pf.scan_num], suf=panels[0])
+                name=name, num=flds[Pf.scan], suf=panels[0])
         print(files)
 
-    def scan_info(self, name):
-        """return scan info matching name"""
-        Pf = ParFields
-        lines = self.matchlines(name)
-        for l in lines:
-            flds = l.split()
-            print(flds[Pf.name], flds[Pf.scan_num], flds[Pf.ostart],\
-              flds[Pf.ostop], flds[Pf.nframes])
-        pass
 
+def _rounde4(x):
+    return int(np.round(1e4*float(x)))
 
 _scanfile_tmpl = "{name}_0{num}.{suf}"
 _imagefiles_tmpl = """
